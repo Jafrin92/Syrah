8edf7039b5e33a91971c59b012e580c5791e80ea
Naima Nusrat Jafrin <naimanusratjafrin@gmail.com>
2022-05-23 21:45:01 +0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// menu
+let mainMenu = $('.custom_nav').offset().top;
+
+$(window).scroll(function(){
+    let ScrollWin = $(window).scrollTop();
+    if(ScrollWin> mainMenu){
+        $('.custom_nav').addClass('menu_fixed');
+    }else{
+        $('.custom_nav').removeClass('menu_fixed');
+    }
+//scroll
+     if(ScrollWin > 200){
+        $('.scroll').fadeIn(1000);
+    }else{
+        $('.scroll').fadeOut(1000);
+    }
+});
+
+//scroll
+    $('.scroll').on("click",function(){
+        $('body,html').animate({
+        scrollTop: 0
+    },500);
+    });
+
+//wow js
+new WOW().init();
